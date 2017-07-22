d25e051172988340424aedf1133159e2cc96b51b
sukanya <sukanya.aneja@gmail.com>
2017-07-22 18:00:55 -0400

    reflection

--- src/lightbox.cpp
+++ src/lightbox.cpp
@@ -11,6 +11,7 @@ struct Sphere {
   Vec3d center;
   Vec3d albedo;
   float radius;
+  float reflectiveness;
 };
 
 struct PointLight {
@@ -22,17 +23,19 @@ struct Plane {
   Vec3d point;
   Vec3d norm;
   Vec3d albedo;
+  float reflectiveness;
 };
 
 struct HitPoint {
   Vec3d point;
   Vec3d norm;
   Vec3d albedo;
+  float reflect;
 };
 
 Vec3d camera;
-
-Sphere spheres[3];
+int sphereCount = 4;
+Sphere spheres[4];
 PointLight plight;
 Plane ground;
 
@@ -97,10 +100,11 @@ Vec3d trace(Vec3d ray_origin, Vec3d ray, int depth) {
       hp.point = hitP;
       hp.norm = norm;
       hp.albedo = ground.albedo;
+      hp.reflect = ground.reflectiveness;
     }
 
     // spheres
-    for(int i=0; i<3; i++) {
+    for(int i=0; i<sphereCount; i++) {
       Vec3d hitP;
       Vec3d norm;
       float hitDist = intersect(spheres[i], ray_origin, ray, hitP, norm);
@@ -111,13 +115,14 @@ Vec3d trace(Vec3d ray_origin, Vec3d ray, int depth) {
         hp.point = hitP;
         hp.norm = norm;
         hp.albedo = spheres[i].albedo;
+        hp.reflect = spheres[i].reflectiveness;
       }
     }
 
 
 
     // pick a random direction
-    L = normalize(Vec3d(rrand()*2-1, rrand()*2-1, rrand()*2-1));
+    L = normalize(Vec3d(rrand(), rrand(), rrand()));
     // ensure that it isn't going inside the sphere  
     // (reflect ray instead?)  
     L = hp.norm.dot(L) < 0 ? -1*L : L;
@@ -129,15 +134,19 @@ Vec3d trace(Vec3d ray_origin, Vec3d ray, int depth) {
     // }
 
     if(hit) {
-      px = trace(hp.point, L, depth-1) * hp.albedo * hp.norm.dot(L);
-      
-      // go hit the light
-      Vec3d hitLight = normalize(plight.center - hp.point);
-      Vec3d lightCol = plight.albedo * hp.norm.dot(hitLight);
+      if(rrand() < hp.reflect) {
+        L = normalize(ray - 2*hp.norm.dot(ray)*hp.norm);
+        px = trace(hp.point, L, depth-1) * hp.albedo * hp.norm.dot(L);
+      } else {
+        px = trace(hp.point, L, depth-1) * hp.albedo * hp.norm.dot(L);
+        // go hit the light
+        Vec3d hitLight = normalize(plight.center - hp.point);
+        Vec3d lightCol = plight.albedo * hp.norm.dot(hitLight);
 
-      // average out ray effects
-      // weight them differently... 
-      px = 0.8*px + 0.2*lightCol;
+        // average out ray effects
+        // weight them differently... 
+        px = 0.8*px + 0.2*lightCol;
+      }      
     } 
     return px;
   }
@@ -154,7 +163,7 @@ how does that relate to right hand / left hand rule for seeing direction of norm
 // The image center will be (0,0), and x will range from -0.5 to 0.5.
 inline Vec3d render_pixel(Vec2d xy) {
   Vec3d px = Vec3d(0.);
-  int count = 100;
+  int count = 160;
   for(int i=0; i<count; i++) {
     Vec3d ray= Vec3d(xy.x, xy.y, 0) - camera;
     Vec3d rayDirection = normalize(ray);
@@ -181,24 +190,33 @@ int main(int argc, char** argv) {
   
   camera = Vec3d(0., 0., 1.);
   
-  spheres[0].center = Vec3d(-0.4, .2, -.7);
-  spheres[0].albedo = Vec3d(0.1, 0., 1.);
-  spheres[0].radius = 0.3;
+  spheres[0].center = Vec3d(-0.65, -.2, -1.5);
+  spheres[0].albedo = Vec3d(.7, .5, .4 );
+  spheres[0].radius = 0.27;
+  spheres[0].reflectiveness = 0.;
 
-  spheres[1].center = Vec3d(0.,0., -.9);
+  spheres[1].center = Vec3d(0.,0., -2.7);
   spheres[1].albedo = Vec3d(.2, .7, .4);
   spheres[1].radius = 0.3;
+  spheres[1].reflectiveness = 0.08;
 
-  spheres[2].center = Vec3d(.85, -.2, -2.6);
-  spheres[2].albedo = Vec3d(.7, .5, .4 );
-  spheres[2].radius = .8;
+  spheres[2].center = Vec3d(1.5, .7, -6.1);
+  spheres[2].albedo = Vec3d(0.9, 0.9, 0.9);
+  spheres[2].radius = .5;
+  spheres[2].reflectiveness = 1.;
+
+  spheres[3].center = Vec3d(.3, -.2, -.9);
+  spheres[3].albedo = Vec3d(0.1, 0.1, 0.8);
+  spheres[3].radius = .1;
+  spheres[3].reflectiveness = .1;
 
   plight.center = Vec3d(-3.,10.,5.);
-  plight.albedo = Vec3d(1.,1.,0.);
+  plight.albedo = Vec3d(1.3,1.3,0.);
 
-  ground.point = Vec3d(0., -.1, 0.);
+  ground.point = Vec3d(0., -.3, 0.);
   ground.norm = normalize(Vec3d(0.,1.,0.));
-  ground.albedo = Vec3d(.2, 0.8, 0.4);
+  ground.albedo = Vec3d(.128, 0.813, 0.291);
+  ground.reflectiveness = .0;
 
   render_image(img);
   

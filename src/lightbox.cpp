11a81fe9496ebb284a50a4377b9c3fadc00e195b
sukanya <sukanya.aneja@gmail.com>
2017-07-22 14:23:25 -0400

    ground, point light

--- src/lightbox.cpp
+++ src/lightbox.cpp
@@ -7,22 +7,46 @@
 using namespace geom;
 using namespace std;
 
-struct sphere {
+struct Sphere {
   Vec3d center;
   Vec3d albedo;
   float radius;
 };
 
-Vec3d camera = Vec3d(0., 0., -1.);
+struct PointLight {
+  Vec3d center;
+  Vec3d albedo;
+};
 
-sphere spheres[3];
+struct Plane {
+  Vec3d point;
+  Vec3d norm;
+  Vec3d albedo;
+};
+
+struct HitPoint {
+  Vec3d point;
+  Vec3d norm;
+  Vec3d albedo;
+};
+
+Vec3d camera;
+
+Sphere spheres[3];
+PointLight plight;
+Plane ground;
 
 float rrand() {
   return (double)rand()/(double)RAND_MAX;
 }
 
-float intersect(sphere s, Vec3d cam, Vec3d raydir) {
-  Vec3d L = s.center - cam;
+Vec3d normalize(Vec3d v) {
+  return v/v.mag();
+}
+
+/* adjust these to assign hit point as well, so its abstracted out */
+float intersect(Sphere s, Vec3d rayorigin, Vec3d raydir, Vec3d &hitP, Vec3d &norm) {
+  Vec3d L = s.center - rayorigin;
   float tca = L.dot(raydir.x, raydir.y, raydir.z);
   if(tca < 0) return -1;
   float d = sqrt(L.mag2() - tca*tca);
@@ -30,49 +54,101 @@ float intersect(sphere s, Vec3d cam, Vec3d raydir) {
   float tch = sqrt(s.radius*s.radius - d*d);
   float t0 = tca - tch;
   float t1 = tca + tch;
-  if(t0<0) return t1;
+  if(t0<0) t0 = t1;
   if(t1<0) return -1;
+  hitP = rayorigin + (t0-0.0001)*raydir; // backup just a bit..
+  norm = normalize(hitP - s.center);
   return t0;
+}
+
+float intersect(Plane p, Vec3d rayorigin, Vec3d raydir, Vec3d &hitP, Vec3d &norm) {
+  float d = p.norm.dot(raydir); 
+  if(abs(d)>0.000001) { // hit plane from either side
+    Vec3d ray = p.point - rayorigin;
+    float t = ray.dot(p.norm) / d;
+    if(t<0) return -1;
+    hitP = rayorigin + (t-0.0001)*raydir; // backup just a bit..
+    norm = p.norm;
+    return t;
+  }
+  return -1;
 }
 
 
 Vec3d trace(Vec3d ray_origin, Vec3d ray, int depth) {
   if(depth == 0) {
-    return Vec3d(.8, .8, .8); // bg
+    //180-233-250
+    return Vec3d(0.705,0.913,.980); // bg
   } else {
-    Vec3d px = Vec3d(.8, .8, .8); // bg
-    Vec3d hitP;
-    Vec3d norm;
+    Vec3d px = Vec3d(0.705,0.913,.980); // bg
+    HitPoint hp;
     Vec3d L;
-    Vec3d lamb;
-    float d = 1000; // ?
+    float d = 1000000000; // ?
     int idx = -1;
     bool hit = false;
+
+    // plane(s)
+    Vec3d hitP;
+    Vec3d norm;
+    float hitDist = intersect(ground, ray_origin, ray, hitP, norm);
+    if(hitDist > 0 && hitDist < d) {
+      d = hitDist;
+      hit = true;
+      hp.point = hitP;
+      hp.norm = norm;
+      hp.albedo = ground.albedo;
+    }
+
+    // spheres
     for(int i=0; i<3; i++) {
-      float hitDist = intersect(spheres[i], ray_origin, ray);
+      Vec3d hitP;
+      Vec3d norm;
+      float hitDist = intersect(spheres[i], ray_origin, ray, hitP, norm);
       if(hitDist > 0 && hitDist < d) {
         d = hitDist;
         idx = i;
         hit = true;
+        hp.point = hitP;
+        hp.norm = norm;
+        hp.albedo = spheres[i].albedo;
       }
     }
-    // back up a bit to ensure point isn't inside the sphere
-    hitP = ray_origin + (d-0.0001)*ray; 
-    // calculate normal
-    norm = hitP - spheres[idx].center;
-    norm /= norm.mag();
+
+
+
     // pick a random direction
-    L = Vec3d(rrand()*2-1, rrand()*2-1, rrand()*2-1);
-    L = L/L.mag();
-    // ensure that it isn't going inside the sphere    
-    L = norm.dot(L) < 0 ? -1*L : L;
+    L = normalize(Vec3d(rrand()*2-1, rrand()*2-1, rrand()*2-1));
+    // ensure that it isn't going inside the sphere  
+    // (reflect ray instead?)  
+    L = hp.norm.dot(L) < 0 ? -1*L : L;
+
+    // // first sphere is reflective
+    // //  put this inside loop so rays can be weighed 
+    // if(idx == 2) {
+    //   L = ray - 2*norm.dot(ray)*norm;
+    // }
+
     if(hit) {
-      px = trace(hitP, L, depth-1) * spheres[idx].albedo * norm.dot(L);
+      px = trace(hp.point, L, depth-1) * hp.albedo * hp.norm.dot(L);
+      
+      // go hit the light
+      Vec3d hitLight = normalize(plight.center - hp.point);
+      Vec3d lightCol = plight.albedo * hp.norm.dot(hitLight);
+
+      // average out ray effects
+      // weight them differently... 
+      px = 0.8*px + 0.2*lightCol;
     } 
     return px;
   }
 }
 
+
+/*
+triangle intersection - barycentric cooridinates
+for normal : use two vectors out from the same point and cross them
+how does that relate to right hand / left hand rule for seeing direction of normal? 
+ */
 
 // Produce a pixel color based on the image coordinates x,y.
 // The image center will be (0,0), and x will range from -0.5 to 0.5.
@@ -81,8 +157,8 @@ inline Vec3d render_pixel(Vec2d xy) {
   int count = 100;
   for(int i=0; i<count; i++) {
     Vec3d ray= Vec3d(xy.x, xy.y, 0) - camera;
-    Vec3d rayDirection = ray/ray.mag();
-    px += trace(camera, rayDirection, 120)/count;
+    Vec3d rayDirection = normalize(ray);
+    px += trace(camera, rayDirection, 20)/count;
   } 
   return px;
 }
@@ -103,21 +179,26 @@ void render_image(Image<double,3>& img) {
 int main(int argc, char** argv) {
   Image<double,3> img(1024, 512); // a 3-channel image.
   
-  // for(int i=0; i<100; i++) {
-  //   cout << (double)rand()/(double)RAND_MAX << endl;
-  // }
+  camera = Vec3d(0., 0., 1.);
   
-  spheres[0].center = Vec3d(-0.4, -.2, .7);
+  spheres[0].center = Vec3d(-0.4, .2, -.7);
   spheres[0].albedo = Vec3d(0.1, 0., 1.);
-  spheres[0].radius = 0.4;
+  spheres[0].radius = 0.3;
 
-  spheres[1].center = Vec3d(.0, .0, 1.);
+  spheres[1].center = Vec3d(0.,0., -.9);
   spheres[1].albedo = Vec3d(.2, .7, .4);
-  spheres[1].radius = 0.35;
+  spheres[1].radius = 0.3;
 
-  spheres[2].center = Vec3d(.55, .3, 1.6);
-  spheres[2].albedo = Vec3d(1., 0.5, 0.4);
-  spheres[2].radius = .6;
+  spheres[2].center = Vec3d(.85, -.2, -2.6);
+  spheres[2].albedo = Vec3d(.7, .5, .4 );
+  spheres[2].radius = .8;
+
+  plight.center = Vec3d(-3.,10.,5.);
+  plight.albedo = Vec3d(1.,1.,0.);
+
+  ground.point = Vec3d(0., -.1, 0.);
+  ground.norm = normalize(Vec3d(0.,1.,0.));
+  ground.albedo = Vec3d(.2, 0.8, 0.4);
 
   render_image(img);
   
